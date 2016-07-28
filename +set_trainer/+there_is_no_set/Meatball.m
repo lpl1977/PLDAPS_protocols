f98fe1af96389ea71cba16e2e1e66a8be5757311
Lee Lovejoy <ll2833@columbia.edu>
2016-07-28 10:19:58 -0400

    subject files too

---
+++ +set_trainer/+there_is_no_set/Meatball.m
@@ -0,0 +1,39 @@
+ function Meatball(p)
+%  TRIAL PARAMETER FILE
+%  PACKAGE:  set_trainer
+%  TRIAL FUNCTION:  there_is_no_set.only_zuul
+%  SUBJECT:  Meatball (aka Spaghetti)
+
+%
+%  REWARDS
+%
+
+%  Reward amounts
+p.trial.stimulus.reward_amount = 0.375;
+
+%
+%  TIMING
+%
+
+p.trial.task.timing.delay.duration = 0.75;
+
+p.trial.task.timing.symbol.duration = 0.75;
+
+p.trial.task.timing.reward_delay.duration = 0.75;
+
+p.trial.task.timing.error_delay.duration = p.trial.task.timing.reward_delay.duration;
+
+%
+%  Training flags
+%
+p.trial.task.training.repeat_errors = true;
+p.trial.task.training.use_symbol_masks = true;
+p.trial.task.training.continue_symbols = true;
+p.trial.task.training.release_for_reward = true;
+p.trial.task.training.shuffle_aborts = false;
+
+
+%
+%  Log contrast for release cue
+%
+p.trial.task.features.log10C = [-1.5 -1.0 -0.5];
