6874ff3b1fed911bec926f3a7db56836ce0d0c32
Lee Lovejoy <ll2833@columbia.edu>
2017-10-29 12:31:57 -0400

    temp update

--- +analogStick/+debug/trialFunction.m
+++ +analogStick/+debug/trialFunction.m
@@ -10,6 +10,16 @@ function p = trialFunction(p,state)
 %  Specific state dependent steps
 switch state
     
+    case p.trial.pldaps.trialStates.experimentPostOpenScreen
+        %  experimentPostOpenScreen--executed once after screen has been
+        %  opened.
+        
+        %  Here is the place to add any windows to the window manager
+        p.functionHandles.windowManagerObj.add('neutral',[2 3],@() p.functionHandles.analogStickObj.position);
+        p.functionHandles.windowManagerObj.add('engaged',[0 0.5],@() p.functionHandles.analogStickObj.position);
+        
+        p.functionHandles.windowManagerObj.disp;
+    
     case p.trial.pldaps.trialStates.trialSetup
         %  Trial Setup, for example starting up Datapixx ADC and allocating
         %  memory
@@ -28,7 +38,9 @@ switch state
 % %             p.trial.pldaps.finish=p.trial.pldaps.iTrial;
 % %         end
 %         
-%     case p.trial.pldaps.trialStates.frameUpdate
+     case p.trial.pldaps.trialStates.frameUpdate
+         
+         %disp([p.functionHandles.analogStickObj.position p.functionHandles.windowManagerObj.in('neutral') p.functionHandles.windowManagerObj.in('engaged')])
 %         %  Frame Update, including getting data from mouse or keyboard and
 %         %  also getting data from Eyelink and Datapixx ADC.  This is where
 %         %  we can check fixation, etc.
